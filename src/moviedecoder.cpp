8527caa204a556f1b71ae6ea383e253ed5135092
Artashes Khanoyan <kh.artash@gmail.com>
2013-07-23 17:29:03 +0400

    Changed variable names in moviedecoder class
    -removed underlines from variable names

--- src/moviedecoder.cpp
+++ src/moviedecoder.cpp
@@ -21,29 +21,29 @@ static const int THUMBNAIL_SIZE=200;
 static const bool MAINTAIN_ASPECT_RATIO = true;
 
 MovieDecoder::MovieDecoder(const std::string& filename, AVFormatContext* pavContext)
-: videoStream_(-1)
-, pFormatContext_(pavContext)
-, pVideoCodecContext_(0)
-, pVideoCodec_(0)
-, pVideoStream_(0)
-, pFrame_(0)
-, pFrameBuffer_(0)
-, pPacket_(0)
-, allowSeek_(true)
+: videoStream(-1)
+, pFormatContext(pavContext)
+, pVideoCodecContext(0)
+, pVideoCodec(0)
+, pVideoStream(0)
+, pFrame(0)
+, pFrameBuffer(0)
+, pPacket(0)
+, allowSeek(true)
 {
     initialize(filename);
 }
 
 MovieDecoder::MovieDecoder()
-: videoStream_(-1)
-, pFormatContext_(0)
-, pVideoCodecContext_(0)
-, pVideoCodec_(0)
-, pVideoStream_(0)
-, pFrame_(0)
-, pFrameBuffer_(0)
-, pPacket_(0)
-, allowSeek_(true)
+: videoStream(-1)
+, pFormatContext(0)
+, pVideoCodecContext(0)
+, pVideoCodec(0)
+, pVideoStream(0)
+, pFrame(0)
+, pFrameBuffer(0)
+, pPacket(0)
+, allowSeek(true)
 {}
 
 MovieDecoder::~MovieDecoder()
@@ -53,7 +53,7 @@ MovieDecoder::~MovieDecoder()
 
 void MovieDecoder::setContext(AVFormatContext *pavContext)
 {
-    pFormatContext_ = pavContext;
+    pFormatContext = pavContext;
 }
 
 void MovieDecoder::initialize(const std::string& filename)
@@ -63,9 +63,9 @@ void MovieDecoder::initialize(const std::string& filename)
 
     std::string inputFile = filename == "-" ? "pipe:" : filename;
 
-    allowSeek_ = filename.find("rtsp://") != 0;
+    allowSeek = filename.find("rtsp://") != 0;
 
-	if (avformat_open_input(&pFormatContext_, inputFile.c_str(), 0, 0) != 0)
+	if (avformat_open_input(&pFormatContext, inputFile.c_str(), 0, 0) != 0)
 	{
 		destroy();
 		throw std::logic_error(std::string("Could not open input file: ") + filename);
@@ -73,49 +73,49 @@ void MovieDecoder::initialize(const std::string& filename)
 
     // This code helps to generate thumbnail for the .avi video files.
     // It successfully generated them on the simulator but crashed on the device.
-//	if (avformat_find_stream_info(pFormatContext_, 0) < 0)
+//	if (avformat_find_stream_info(pFormatContext, 0) < 0)
 //	{
 //		destroy();
 //		throw std::logic_error(std::string("Could not find stream information"));
 //	}
     initializeVideo();
-    pFrame_ = avcodec_alloc_frame();
+    pFrame = avcodec_alloc_frame();
 }
 
 void MovieDecoder::destroy()
 {
-    if (pVideoCodecContext_)
+    if (pVideoCodecContext)
     {
-        avcodec_close(pVideoCodecContext_);
-        pVideoCodecContext_ = 0;
+        avcodec_close(pVideoCodecContext);
+        pVideoCodecContext = 0;
     }
 
 
-    if (pFormatContext_)
+    if (pFormatContext)
     {
-        avformat_close_input(&pFormatContext_);
+        avformat_close_input(&pFormatContext);
     }
 
-    if (pPacket_)
+    if (pPacket)
     {
-        av_free_packet(pPacket_);
-        delete pPacket_;
-        pPacket_ = 0;
+        av_free_packet(pPacket);
+        delete pPacket;
+        pPacket = 0;
     }
 
-    if (pFrame_)
+    if (pFrame)
     {
-        av_free(pFrame_);
-        pFrame_ = 0;
+        av_free(pFrame);
+        pFrame = 0;
     }
 
-    if (pFrameBuffer_)
+    if (pFrameBuffer)
     {
-        av_free(pFrameBuffer_);
-        pFrameBuffer_ = 0;
+        av_free(pFrameBuffer);
+        pFrameBuffer = 0;
     }
 
-    videoStream_ = -1;
+    videoStream = -1;
 }
 
 void MovieDecoder::decodeVideoFrame()
@@ -134,7 +134,7 @@ void MovieDecoder::decodeVideoFrame()
 
 void MovieDecoder::seek(int timeInSeconds)
 {
-    if (!allowSeek_)
+    if (!allowSeek)
     {
         return;
     }
@@ -146,10 +146,10 @@ void MovieDecoder::seek(int timeInSeconds)
         timestamp = 0;
     }
 
-    int ret = av_seek_frame(pFormatContext_, -1, timestamp, 0);
+    int ret = av_seek_frame(pFormatContext, -1, timestamp, 0);
     if (ret >= 0)
     {
-        avcodec_flush_buffers(pFormatContext_->streams[videoStream_]->codec);
+        avcodec_flush_buffers(pFormatContext->streams[videoStream]->codec);
     }
     else
     {
@@ -176,7 +176,7 @@ void MovieDecoder::seek(int timeInSeconds)
         }
 
         ++keyFrameAttempts;
-    } while ((!gotFrame || !pFrame_->key_frame) && keyFrameAttempts < 200);
+    } while ((!gotFrame || !pFrame->key_frame) && keyFrameAttempts < 200);
 
     if (gotFrame == 0)
     {
@@ -186,7 +186,7 @@ void MovieDecoder::seek(int timeInSeconds)
 
 int MovieDecoder::getDuration(const std::string& videoFile)
 {
-    if (pFormatContext_)
+    if (pFormatContext)
     {
     	bb::data::JsonDataAccess jda;
     	QString m_file = QDir::home().absoluteFilePath("videoInfoList.json");
@@ -219,20 +219,20 @@ int MovieDecoder::getDuration(const std::string& videoFile)
 
 int MovieDecoder::getWidth()
 {
-	return pVideoCodecContext_->width;
+	return pVideoCodecContext->width;
 }
 
 int MovieDecoder::getHeight()
 {
-	return pVideoCodecContext_->height;
+	return pVideoCodecContext->height;
 }
 
 void MovieDecoder::getScaledVideoFrame(VideoFrame& videoFrame)
 {
-    if (pFrame_->interlaced_frame)
+    if (pFrame->interlaced_frame)
     {
-        avpicture_deinterlace((AVPicture*) pFrame_, (AVPicture*) pFrame_, pVideoCodecContext_->pix_fmt,
-                              pVideoCodecContext_->width, pVideoCodecContext_->height);
+        avpicture_deinterlace((AVPicture*) pFrame, (AVPicture*) pFrame, pVideoCodecContext->pix_fmt,
+                              pVideoCodecContext->width, pVideoCodecContext->height);
     }
 
     int scaledWidth, scaledHeight;
@@ -240,44 +240,44 @@ void MovieDecoder::getScaledVideoFrame(VideoFrame& videoFrame)
 
     videoFrame.width = scaledWidth;
     videoFrame.height = scaledHeight;
-    videoFrame.lineSize = pFrame_->linesize[0];
+    videoFrame.lineSize = pFrame->linesize[0];
 
     videoFrame.frameData.clear();
     videoFrame.frameData.resize(videoFrame.lineSize * videoFrame.height);
-    memcpy((&(videoFrame.frameData.front())), pFrame_->data[0], videoFrame.lineSize * videoFrame.height);
+    memcpy((&(videoFrame.frameData.front())), pFrame->data[0], videoFrame.lineSize * videoFrame.height);
 }
 
 void MovieDecoder::initializeVideo()
 {
-    for (unsigned int i = 0; i < pFormatContext_->nb_streams; ++i)
+    for (unsigned int i = 0; i < pFormatContext->nb_streams; ++i)
     {
-        if (pFormatContext_->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO)
+        if (pFormatContext->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO)
         {
-            pVideoStream_ = pFormatContext_->streams[i];
-            videoStream_ = i;
+            pVideoStream = pFormatContext->streams[i];
+            videoStream = i;
             break;
         }
     }
 
-    if (videoStream_ == -1)
+    if (videoStream == -1)
     {
         throw std::logic_error("Could not find video stream");
     }
 
-    pVideoCodecContext_ = pFormatContext_->streams[videoStream_]->codec;
-    pVideoCodec_ = avcodec_find_decoder(pVideoCodecContext_->codec_id);
+    pVideoCodecContext = pFormatContext->streams[videoStream]->codec;
+    pVideoCodec = avcodec_find_decoder(pVideoCodecContext->codec_id);
 
-    if (pVideoCodec_ == 0)
+    if (pVideoCodec == 0)
     {
     	fprintf(stderr, "Codec is not found :((((((");
-        // set to NULL, otherwise avcodec_close(pVideoCodecContext_) crashes
-        pVideoCodecContext_ = 0;
+        // set to NULL, otherwise avcodec_close(pVideoCodecContext) crashes
+        pVideoCodecContext = 0;
         throw std::logic_error("Video Codec not found");
     }
 
-    pVideoCodecContext_->workaround_bugs = 1;
+    pVideoCodecContext->workaround_bugs = 1;
 
-	if (avcodec_open2(pVideoCodecContext_, pVideoCodec_, 0) < 0)
+	if (avcodec_open2(pVideoCodecContext, pVideoCodec, 0) < 0)
     {
 		fprintf(stderr, "Successfully opened video codec...");
         throw std::logic_error("Could not open video codec");
@@ -291,23 +291,23 @@ bool MovieDecoder::getVideoPacket()
 
     int attempts = 0;
 
-    if (pPacket_ !=0)
+    if (pPacket !=0)
     {
-        av_free_packet(pPacket_);
-        delete pPacket_;
+        av_free_packet(pPacket);
+        delete pPacket;
     }
 
-    pPacket_ = new AVPacket();
+    pPacket = new AVPacket();
 
     while (framesAvailable && !frameDecoded && (attempts++ < 1000))
     {
-        framesAvailable = av_read_frame(pFormatContext_, pPacket_) >= 0;
+        framesAvailable = av_read_frame(pFormatContext, pPacket) >= 0;
         if (framesAvailable)
         {
-            frameDecoded = pPacket_->stream_index == videoStream_;
+            frameDecoded = pPacket->stream_index == videoStream;
             if (!frameDecoded)
             {
-                av_free_packet(pPacket_);
+                av_free_packet(pPacket);
             }
         }
     }
@@ -317,16 +317,16 @@ bool MovieDecoder::getVideoPacket()
 
 bool MovieDecoder::decodeVideoPacket()
 {
-	if (pPacket_->stream_index != videoStream_)
+	if (pPacket->stream_index != videoStream)
 	{
 		return false;
 	}
 
-    avcodec_get_frame_defaults(pFrame_);
+    avcodec_get_frame_defaults(pFrame);
 
     int frameFinished;
 
-    int bytesDecoded = avcodec_decode_video2(pVideoCodecContext_, pFrame_, &frameFinished, pPacket_);
+    int bytesDecoded = avcodec_decode_video2(pVideoCodecContext, pFrame, &frameFinished, pPacket);
     if (bytesDecoded < 0)
     {
         throw std::logic_error("Failed to decode video frame: bytesDecoded < 0");
@@ -339,8 +339,8 @@ void MovieDecoder::convertAndScaleFrame(PixelFormat format, int& scaledWidth, in
 {
     calculateDimensions(scaledWidth, scaledHeight);
 
-    SwsContext* scaleContext = sws_getContext(pVideoCodecContext_->width, pVideoCodecContext_->height,
-                                              pVideoCodecContext_->pix_fmt, scaledWidth, scaledHeight,
+    SwsContext* scaleContext = sws_getContext(pVideoCodecContext->width, pVideoCodecContext->height,
+                                              pVideoCodecContext->pix_fmt, scaledWidth, scaledHeight,
                                               format, SWS_BICUBIC, 0, 0, 0);
     if (0 == scaleContext)
     {
@@ -352,14 +352,14 @@ void MovieDecoder::convertAndScaleFrame(PixelFormat format, int& scaledWidth, in
 
     createAVFrame(&convertedFrame, &convertedFrameBuffer, scaledWidth, scaledHeight, format);
 
-    sws_scale(scaleContext, pFrame_->data, pFrame_->linesize, 0, pVideoCodecContext_->height,
+    sws_scale(scaleContext, pFrame->data, pFrame->linesize, 0, pVideoCodecContext->height,
               convertedFrame->data, convertedFrame->linesize);
     sws_freeContext(scaleContext);
 
-    av_free(pFrame_);
-    av_free(pFrameBuffer_);
-    pFrame_ = convertedFrame;
-    pFrameBuffer_  = convertedFrameBuffer;
+    av_free(pFrame);
+    av_free(pFrameBuffer);
+    pFrame = convertedFrame;
+    pFrameBuffer  = convertedFrameBuffer;
 }
 
 void MovieDecoder::calculateDimensions(int& destWidth, int& destHeight)
@@ -372,10 +372,10 @@ void MovieDecoder::calculateDimensions(int& destWidth, int& destHeight)
     }
     else
     {
-        int srcWidth            = pVideoCodecContext_->width;
-        int srcHeight           = pVideoCodecContext_->height;
-        int ascpectNominator    = pVideoCodecContext_->sample_aspect_ratio.num;
-        int ascpectDenominator  = pVideoCodecContext_->sample_aspect_ratio.den;
+        int srcWidth            = pVideoCodecContext->width;
+        int srcHeight           = pVideoCodecContext->height;
+        int ascpectNominator    = pVideoCodecContext->sample_aspect_ratio.num;
+        int ascpectDenominator  = pVideoCodecContext->sample_aspect_ratio.den;
 
         if (ascpectNominator != 0 && ascpectDenominator != 0)
         {
