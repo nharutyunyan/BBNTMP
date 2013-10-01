384e777f7aa424f726a2e0874f3925c474c6111c
Suren6212 <sur6212gmail.com>
2013-10-01 15:16:35 +0400

    Vibration for small slider
    Added a haptic feedback (short vibration)
    when the small slider is opened on the
    video player page.

--- src/BpsEventHandler.hpp
+++ src/BpsEventHandler.hpp
@@ -24,7 +24,7 @@ public:
     virtual void event(bps_event_t *event);
     Q_INVOKABLE void onVolumeValueChanged(float volumeSliderValue);
     Q_INVOKABLE float getVolume();
-    Q_INVOKABLE void startVibration();
+    Q_INVOKABLE void startVibration(int intensity, int duration);
 
 signals:
   	void speakerVolumeChanged(float speakerVolume);
